8cf2cc5d589c28af6af7b48778603efae056b879
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-06 16:56:51 -0300

    implement view model to main activity

--- app/src/main/java/com/lucasmezencio/vmodel/MainActivity.kt
+++ app/src/main/java/com/lucasmezencio/vmodel/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,43 +14,36 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnDados: Button
     lateinit var btnMostrar: Button
 
-    var contador: Int = 0
+    lateinit var mViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initDados()
-        initContador()
         initClick()
 
-        validaContador()
     }
 
-    private fun validaContador() {
-        if (contador > 5)
-            contador = 0
+    private fun initDados() {
+        mViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+
+        mViewModel.mContador.observe(this, Observer { valor ->
+            txtContador.setText(valor)
+        })
     }
 
     private fun initClick() {
         btnDados.setOnClickListener {
-            contador++
-            validaContador()
-            initContador()
+            mViewModel.contador()
         }
 
         btnMostrar.setOnClickListener {
-            Toast.makeText(this, "Valor contador: $contador", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Valor contador: ${mViewModel.mContador.value}", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun initContador() {
-        txtContador.setText(contador.toString())
-    }
-
-    private fun initDados() {
-        txtContador = findViewById(R.id.txtContador)
-        btnDados = findViewById(R.id.btnDados)
-        btnMostrar = findViewById(R.id.btnMostrar)
     }
 }
---
+++ app/src/main/java/com/lucasmezencio/vmodel/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.lucasmezencio.vmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var mContador = MutableLiveData<String>().apply { value = contador.toString() }
+
+    private var contador: Int = 0
+
+    private fun setMContador() {
+        mContador.value = contador.toString()
+    }
+
+    private fun validaContador() {
+        contador++
+        if (contador > 5) {
+            contador = 0
+        }
+
+        setMContador()
+    }
+
+    fun contador() {
+        validaContador()
+    }
+}
